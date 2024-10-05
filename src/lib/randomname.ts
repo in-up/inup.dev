f0e9371f4078f5793a89554677010796aae483f7
in-up <inup@kyonggi.ac.kr>
2024-10-06 08:03:36 +0900

    feat: add random name generator

---
+++ src/lib/randomname.ts
@@ -0,0 +1,38 @@
+const adjectives = [
+    '편안한', 
+    '행복한', 
+    '즐거운',
+    '귀여운',
+    '용감한',
+    '뛰어난',
+    '똑똑한',
+    '엉뚱한',
+    '근사한',
+    '침착한',
+    '명량한'
+];
+const animals = [
+    '토끼', 
+    '리트리버', 
+    '고양이',
+    '쿼카',
+    '너구리',
+    '래서판다',
+    '다람쥐',
+    '모래여우',
+    '뱁새',
+    '펭귄',
+    '호박벌',
+    '파랑어치'
+];
+
+const getRandomElem = <T>(arr: T[]): T => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+};
+
+export const generateName = (): string => {
+    const adjective = getRandomElem(adjectives);
+    const animal = getRandomElem(animals);
+    return `${adjective} ${animal}`;
+};
