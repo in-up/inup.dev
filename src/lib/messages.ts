7eeb2b0465928eb63ea9f0727c2d58c99e67fd2d
in-up <inup@kyonggi.ac.kr>
2024-11-02 03:17:00 +0900

    style: error message edited

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -1,10 +1,10 @@
 export const messages = {
     comment: {
       promptInfo: "삭제 코드를 입력하세요.\n이 작업은 되돌릴 수 없습니다.",
-      copied: "삭제 코드가 복사되었습니다.",
+      copied: "삭제 코드를 복사했습니다.",
       deleted: "댓글이 삭제되었습니다.",
       deleteValidationFailed: "삭제 코드가 일치하지 않습니다.",
-      error: "알 수 없는 오류가 발생했습니다.",
+      error: "오류가 발생했습니다. 다시 시도해주세요.",
       fetchError: "댓글을 불러오는 데 실패했습니다.",
       deleteError: "삭제 중 오류가 발생했습니다. 다시 시도해주세요.",
       hashingError: "암호화 중 오류가 발생했습니다. 다시 시도해주세요.",
