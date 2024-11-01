e4e4d99dcc8a1e803637f5449ceff22d9b6198b2
in-up <inup@kyonggi.ac.kr>
2024-11-02 03:52:42 +0900

    fix: modules and scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,29 @@
-const { withContentlayer } = require("next-contentlayer2"); // eslint-disable-line
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "dgtzuqphqg23d.cloudfront.net",
-//       },
-//       { protocol: "https", hostname: "image.mux.com" },
-//     ],
+const nextConfig = {};
+
+export default nextConfig;
+
+// import { withContentlayer } from "next-contentlayer2"; // eslint-disable-line
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // images: {
+//   //   remotePatterns: [
+//   //     {
+//   //       protocol: "https",
+//   //       hostname: "dgtzuqphqg23d.cloudfront.net",
+//   //     },
+//   //     { protocol: "https", hostname: "image.mux.com" },
+//   //   ],
+//   // },
+//   webpack: (config, { isServer }) => {
+//     if (isServer) {
+//       import("./scripts/generate-sitemap.js");
+//       import("./scripts/generate-rss.js");
+//     }
+
+//     return config;
 //   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require("./scripts/generate-sitemap");
-      require("./scripts/generate-rss");
-    }
+// };
 
-    return config;
-  },
-};
-
-module.exports = withContentlayer(nextConfig);
+// export default withContentlayer(nextConfig);
